a5f957f282c940d9b7259d604183c6f7beb5ebc3
samuelsunil <sunilvsamuel@gmail.com>
2022-02-28 08:19:15 +0530

    New post Slug edit

--- app/utils/mdx.tsx
+++ app/utils/mdx.tsx
@@ -1,10 +1,11 @@
 
-import type { MdxListItem, MdxPage} from '~/types'
+import type { GitHubFile,MdxListItem, MdxPage} from '~/types'
 import { typedBoolean } from '~/utils/misc'
 import {
     downloadDirList,
     downloadMdxFileOrDirectory,
   } from '~/utils/github.server'
+  import {compileMdx} from '~/utils/compile-mdx.server'
 
 /**
  * This is useful for when you don't want to send all the code for a page to the client.
@@ -56,15 +57,68 @@ async function getMdxPagesInDirectory(
       }),
     )
   
-    // const pages = await Promise.all(
-    //   pageDatas.map(pageData =>
-    //     compileMdxCached({contentDir, ...pageData}),
-    //   ),
-    // )
+    const pages = await Promise.all(
+      pageDatas.map(pageData =>
+        compileMdxCached({contentDir, ...pageData}),
+      ),
+    )
  
-    return pageDatas // pages.filter(typedBoolean)
+    return  pages.filter(typedBoolean)
   }
 
+  async function compileMdxCached({
+    contentDir,
+    slug,
+    entry,
+    files,
+  }: {
+    contentDir: string
+    slug: string
+    entry: string
+    files: Array<GitHubFile>
+  }) {
+   
+        const compiledPage = await compileMdx<MdxPage['frontmatter']>(slug, files)
+        if (compiledPage) {
+        //   if (
+        //     compiledPage.frontmatter.bannerCloudinaryId &&
+        //     !compiledPage.frontmatter.bannerBlurDataUrl
+        //   ) {
+        //     try {
+        //       compiledPage.frontmatter.bannerBlurDataUrl = await getBlurDataUrl(
+        //         compiledPage.frontmatter.bannerCloudinaryId,
+        //       )
+        //     } catch (error: unknown) {
+        //       console.error(
+        //         'oh no, there was an error getting the blur image data url',
+        //         error,
+        //       )
+        //     }
+        //   }
+        //   if (compiledPage.frontmatter.bannerCredit) {
+        //     const credit = await markdownToHtmlUnwrapped(
+        //       compiledPage.frontmatter.bannerCredit,
+        //     )
+        //     compiledPage.frontmatter.bannerCredit = credit
+        //     const noHtml = await stripHtml(credit)
+        //     if (!compiledPage.frontmatter.bannerAlt) {
+        //       compiledPage.frontmatter.bannerAlt = noHtml
+        //         .replace(/(photo|image)/i, '')
+        //         .trim()
+        //     }
+        //     if (!compiledPage.frontmatter.bannerTitle) {
+        //       compiledPage.frontmatter.bannerTitle = noHtml
+        //     }
+        //   }
+          return {
+            ...compiledPage,
+            slug,
+            editLink: `https://github.com/samuelsunil/sunilsamuel-site/edit/main/${entry}`,
+          }
+        } else {
+          return null
+        }
+  }
   
 
 async function getBlogMdxListItems() {
