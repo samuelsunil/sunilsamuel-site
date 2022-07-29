f6944fe7b4ab6e0d5e2b4cc73993782e900ca4be
samuelsunil <sunilvsamuel@gmail.com>
2022-07-29 08:32:38 +0530

    Remove draft from blog

--- app/refresh.ignored.js
+++ app/refresh.ignored.js
@@ -1,1 +1,1 @@
-// 1659063491783: /Users/sunilsamuel/GIT/sunilsamuel-site/content/blog/inner-critic-vs-inner-mentor.mdx
+// 1659063741565: /Users/sunilsamuel/GIT/sunilsamuel-site/content/blog/inner-critic-vs-inner-mentor.mdx
